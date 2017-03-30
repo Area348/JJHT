3258f6a70d44f651f53c284c66991c900d6bced9
Area348 <github@area348.nl>
2017-03-30 15:51:28 +0200

    FullMoon.java
    HeadFirst pagina 312.

---
+++ FullMoons.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.System.out;
+
+class FullMoons 
+{
+	
+	static int DAY_IN = 1000 * 60 * 60 * 24;
+	
+	public static void main(String[] arg)
+	{
+		
+		
+		Calendar c = Calendar.getInstance();
+		
+		c.set(2017,0,12,12,34);
+		
+		out.println("Start: " + c);
+		out.println();
+		out.println("-----------");
+		out.println();
+		
+		out.println(String.format("Full moon on %tc", c));
+		
+		long day1 = c.getTimeInMillis();
+		
+				
+				
+		for(int x = 0; x < 60; x++)
+		{
+			day1 += (DAY_IN * 29.5);
+			c.setTimeInMillis(day1);
+
+
+			out.println(String.format("Full moon on %tc", c));
+		
+		
+		}
+
+			
+		
+	}
+	
+	
+	
+	
+	
+}
